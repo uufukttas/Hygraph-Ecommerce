6692ec8c3d9a0ae2e35741278b09a06ae1906c2b
UFUK TAS <uufukttas@hotmail.com>
2023-02-06 22:56:15 +0300

    Fixed eslint file

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.graphassets.com',
-      },
-    ],
-  },
-  eslint: {
-    dirs: ["pages"]
-  },
-  experimental: {
-    forceSwcTransforms: true,
+  extends: ['next/core-web-vitals', 'turbo', 'prettier'],
+  ignorePatterns: ['node_modules', 'dist'],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
   },
 }
 
